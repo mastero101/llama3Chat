4dff127ea98b34a865285474e03d67c959396022
mastero101 <mastershift_101@hotmail.com>
2024-05-04 04:03:57 -0600

    change on endpoint

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -41,7 +41,7 @@ export class ChatComponent {
 
       const messageObject = { message: this.newMessage };
       this.newMessage = '';
-      axios.post('http://localhost:3000/sendMessage', messageObject)
+      axios.post('http://132.145.206.61:5600/sendMessage', messageObject)
         .then((response: any) => {
           if (response.data && response.data[0] && response.data[0].response) {
             const receivedMessage = response.data[0].response.response;
